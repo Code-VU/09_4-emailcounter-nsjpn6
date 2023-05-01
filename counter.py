aafd3b67f3a6e742510ab9e1f83f1f2cb37d5378
nsjpn6 <122318625+nsjpn6@users.noreply.github.com>
2023-05-01 11:54:42 -0500

    updated

--- counter.py
+++ counter.py
@@ -1,8 +1,46 @@
 def countEmail():
     # This first line is provided for you
-    name = input("Enter file:")
-    if len(name) < 1 : name = "mbox-short.txt"
-    handle = open(name)
+    # slide 28 for count
+    # 
+    file_name = input("Enter file:")
+    if len(file_name) < 1 : file_name = "mbox-short.txt"
+    fromDict = {}
+    
+    handle = open(file_name)
+    for line in handle :
+        if line.startswith("From "):
+            sender = line.split()[1]
+            
+            if sender not in fromDict:
+                fromDict[sender] = 1
+            else:
+                fromDict[sender] += 1
+
+    dictVals = fromDict.values()
+    maxVal = max(dictVals)
+
+    print(max(fromDict, key=fromDict.get), maxVal)
+   
+
+
+    
+
+    
+
+
+
+
+
+        
+
+                    
+                
+
+
+
+
+  
+                
         
 
 ## if you want to test locally run > python counter.py
